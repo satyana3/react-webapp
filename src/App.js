8964d19be3f86abd8a17997aed85d417b8bff788
satyana3 <satyana3@gmail.com>
2024-07-19 08:48:39 +0530

    Update App.js
    Latest

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
           >
           </a>
           <h3>
-            CloudAcademy ❤ DevOps 2016-2024 Version 2.0
+            CloudAcademy ❤ DevOps 2016-24 Version 2.0
           </h3>
         </header>
       </div>
